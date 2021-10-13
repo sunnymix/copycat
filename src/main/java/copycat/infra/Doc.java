1c9c762d7d6e7d313b5d5fec82ee1f5dff370608
sunny <sunny@sunnymix.com>
2021-10-14 00:03:04 +0800

    Add Doc

---
+++ src/main/java/copycat/infra/Doc.java
@@ -0,0 +1,41 @@
+package copycat.infra;
+
+public class Doc {
+    public static void main(String[] args) {
+        new Doc("/tmp/copycat/doc", "test", Ext.MD).save("# Test");
+    }
+
+    private String dir;
+
+    private String name;
+
+    private Ext ext;
+
+    public Doc(String dir, String name, Ext ext) {
+        this.dir = dir;
+        this.name = name;
+        this.ext = ext;
+    }
+
+    public void save(String content) {
+        System.out.printf("Save file: %s%n", filePath());
+    }
+
+    public String filePath() {
+        return String.format("%s/%s.%s", dir, name, ext.id);
+    }
+
+    public enum Ext {
+        MD("md");
+
+        private final String id;
+
+        Ext(String id) {
+            this.id = id;
+        }
+
+        public String getText() {
+            return id;
+        }
+    }
+}
